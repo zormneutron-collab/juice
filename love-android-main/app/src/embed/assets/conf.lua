1f8eb4c4e295aed4f7c7ee1e982077014df3fb43
zormneutron-collab <zormneutron@gmail.com>
2026-02-20 11:39:17 +0200

    Update conf.lua

--- love-android-main/app/src/embed/assets/conf.lua
+++ love-android-main/app/src/embed/assets/conf.lua
@@ -2,8 +2,10 @@ function love.conf(t)
     t.modules.joystick = false
     t.modules.physics = false
     t.externalstorage = true
-    t.version = "11.5"
+    t.version = "12.0"
+    t.window.display = 0
 
-    -- أضف هذا السطر فقط للتأكيد البرمجي
-    t.window.display = 0 
+    -- ✅ أضف هذا:
+    t.window.width = 480
+    t.window.height = 854
 end
